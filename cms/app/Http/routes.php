6d125f7de8e5679955beef64b6810439320bb1f9
Felipe Rocha <felipenrocha19@gmail.com>
2019-06-18 15:47:57 -0300

    First commit

---
+++ cms/app/Http/routes.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+    // return "Hi you";
+});
+
+// Route::get('/post/{id}', function ($id) { 
+//     return $id;  
+// });
+
+// Route::get('admin/posts/example', array('as'=>'admin.home', function() {
+//     $url = route('admin.home');
+//     return $url;
+// }));
+// Route::group(['middleware' => ['web']], function () {
+
+// });
+
+
+// Route::get('/post/{id}', 'PostsController@index');
+
+Route::resource('posts', 'PostsController');
